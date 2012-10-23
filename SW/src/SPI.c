3717f63a222d35eb1340a109cd998c654505716c
unknown <the.n4a@gmail.com>
2012-10-23 15:40:59 +0200

    added TIM.h for Delay function required

--- SW/src/SPI.c
+++ SW/src/SPI.c
@@ -9,7 +9,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x.h"
 #include "SPI.h"
-#include "TIM.h"
+#include "TIM.h"	//function Delay defined in TIM.c
 
 
 
